f48db61df5725bb1b0dae73447c73306fd604d82
ivan <c.ivan.felix.r@gmail.com>
2019-12-08 13:26:53 -0600

    hello world in unity

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelloWorld : MonoBehaviour
+{
+    public InputField nameField;
+    public GameObject popupPanel;
+    public Text messageText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	hidePopupPanel ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void hidePopupPanel(){
+        popupPanel.SetActive (false);
+    }
+
+    public void showPopupPanel(){
+        popupPanel.SetActive (true);
+    }
+
+    public void sayHello(){
+	messageText.text = "Hello " + nameField.text + "!";
+	showPopupPanel ();
+    }
+}
